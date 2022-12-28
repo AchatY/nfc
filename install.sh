5fcb690a9eddf7b93a0b784156588996e591b9dc
Achat Youmer <achat_y@etna-alternance.net>
2022-12-28 12:21:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 wget --no-check-certificate -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
 
-apt update
-
 apt update --fix-missing
 
-apt-get install libnfc-examples
+# apt-get install libnfc-examples
