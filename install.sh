3f9edcabf8e57f6f480d40482e8680787286396f
Achat Youmer <achat_y@etna-alternance.net>
2022-12-28 11:37:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
+wget -O kali-archive-keyring_2020.2_all.deb https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2020.2_all.deb
+dpkg -i kali-archive-keyring_2020.2_all.deb
+apt update
+apt full-upgrade
 
 # update
 apt-get update
