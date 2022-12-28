bf1bc731defb48c59581f0511d3ec08349151b6d
Achat Youmer <achat_y@etna-alternance.net>
2022-12-28 11:11:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# download
+wget http://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2022.1_all.deb
+# install
+dpkg -i kali-archive-keyring_2022.1_all.deb
+# remove downloaded file again
+rm kali-archive-keyring_2022.1_all.deb
+# update
 apt-get update
+#
 apt-get install libusb-dev
 apt-get install libnfc-bin libnfc-dev libnfc5
 apt-get install libnfc-examples
