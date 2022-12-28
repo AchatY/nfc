4bbe0e6f979b26a07c7cfa71d286b9c830222d8f
Achat Youmer <achat_y@etna-alternance.net>
2022-12-28 11:25:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# download
-wget http://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2022.1_all.deb
-# install
-dpkg -i kali-archive-keyring_2022.1_all.deb
-# remove downloaded file again
-rm kali-archive-keyring_2022.1_all.deb
+wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
+
 # update
 apt-get update
 #
